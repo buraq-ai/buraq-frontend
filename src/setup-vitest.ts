7f52b0405debe6618e6102d8dbf5251797b386d4
Noureddineblbli <nourddineblibli78@gmail.com>
2026-05-30 16:49:35 +0100

    BURAQ-88: Fix Angular 21 test environment setup

---
+++ src/setup-vitest.ts
@@ -0,0 +1,10 @@
+import 'zone.js';
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { ApplicationRef, NgZone } from '@angular/core';
+
+TestBed.initTestEnvironment(
+  BrowserModule,
+  TestBed as any
+);
